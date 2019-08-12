85ee5799f1b6add4f2633195a76b30367ca5d142
Anthony <anton.p@qualisystems.com>
2019-08-12 16:46:31 +0300

    update Chassis autoload with "Serial Number", "Model Name" and "OS Version" attributes

--- cgs/flows/autoload.py
+++ cgs/flows/autoload.py
@@ -1,3 +1,5 @@
+import re
+
 from cloudshell.layer_one.core.response.response_info import ResourceDescriptionResponseInfo
 from cloudshell.layer_one.core.response.resource_info.entities.chassis import Chassis
 from cloudshell.layer_one.core.response.resource_info.entities.port import Port
@@ -23,13 +25,16 @@ class AutoloadFlow(object):
         with self._cli_handler.enable_mode_service() as cli_service:
             autoload_actions = AutoloadActions(cli_service=cli_service, logger=self._logger)
 
-            # serial_number = autoload_actions.get_switch_serial()
-            # switch_details = autoload_actions.get_switch_details()
+            switch_details = autoload_actions.get_switch_details()
+            switch_details = re.search(r"[Dd]evice\s+[Mm]odel\s+(?P<device_model>.*)\n"
+                                       r"[Ss]erial\s+[Nn]umber\s+(?P<serial_number>.*)\n"
+                                       r"[Ss][Ww]\s[Vv]ersion\s+(?P<sw_version>.*)\n",
+                                       switch_details)
 
             chassis = Chassis(resource_id="", address=address, model_name="Cgs Chassis")
-            # chassis.set_model_name("")
-            # chassis.set_serial_number("")
-            # chassis.set_os_version("")
+            chassis.set_model_name(switch_details.group("device_model"))
+            chassis.set_serial_number(switch_details.group("serial_number"))
+            chassis.set_os_version(switch_details.group("sw_version"))
 
             for port in autoload_actions.get_ports():
                 port_resource = Port(resource_id=port.port_id.replace("/", "-"))
