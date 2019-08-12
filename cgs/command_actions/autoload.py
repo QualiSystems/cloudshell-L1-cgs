cc1d8566ca4241b230f61a233e76afdd8d927a55
Anthony <anton.p@qualisystems.com>
2019-08-12 15:42:08 +0300

    add ConsoleTable class

--- cgs/command_actions/autoload.py
+++ cgs/command_actions/autoload.py
@@ -3,9 +3,8 @@ import re
 from cloudshell.cli.command_template.command_template_executor import CommandTemplateExecutor
 
 from cgs.command_templates import autoload
-from cgs.helpers.errors import ParseFilterError
 from cgs.helpers.errors import UnsupportedPortsInFilterError
-from cgs.helpers.table2dicts import ParseTableError, table2dicts
+from cgs.helpers.table2dicts import ConsoleTable
 
 
 class AutoloadActions(object):
@@ -32,103 +31,57 @@ class AutoloadActions(object):
         return Ports(self._logger, ports)
 
 
-# todo: create base class for filters/ports - ConsoleTable
-class Port(object):
-    ADMIN_ENABLED = "enabled"
-    ACTION_REDIRECT = "redirect"
-    PORT_PATTERN = re.compile(r"^\d+(/\d+)?$")
+class Ports(ConsoleTable):
+    class Model(ConsoleTable.Model):
+        ADMIN_ENABLED = "enabled"
+        ACTION_REDIRECT = "redirect"
+        PORT_PATTERN = re.compile(r"^\d+(/\d+)?$")
 
-    def __init__(self, port_id, admin, speed):
-        """
+        def __init__(self, port_id, admin, speed):
+            """
 
-        :param str port_id:
-        :param str admin:
-        :param str speed:
-        """
-        self.port_id = port_id
-        self.admin = admin
-        self.speed = speed
-        self.validate()
+            :param str port_id:
+            :param str admin:
+            :param str speed:
+            """
+            self.port_id = port_id
+            self.admin = admin
+            self.speed = speed
+            self.validate()
 
-    @classmethod
-    def from_dict(cls, data):
-        """
+        @classmethod
+        def from_dict(cls, data):
+            """
 
-        :param dict data:
-        :return:
-        """
-        return cls(
-            port_id=data["Port"],
-            admin=data["Admin"],
-            speed=data["Speed"]
-        )
+            :param dict data:
+            :return:
+            """
+            return cls(
+                port_id=data["Port"],
+                admin=data["Admin"],
+                speed=data["Speed"]
+            )
 
-    @property
-    def is_enabled(self):
-        """
+        @property
+        def is_enabled(self):
+            """
 
-        :rtype: bool
-        """
-        return self.admin.lower() == self.ADMIN_ENABLED
+            :rtype: bool
+            """
+            return self.admin.lower() == self.ADMIN_ENABLED
 
-    def validate(self):
-        """
+        def validate(self):
+            """
 
-        :return:
-        """
-        self.validate_port(self.port_id)
+            :return:
+            """
+            self.validate_port(self.port_id)
 
-    def validate_port(self, port):
-        """
+        def validate_port(self, port):
+            """
 
-        :param str port:
-        :return:
-        """
-        if not self.PORT_PATTERN.match(port):
-            raise UnsupportedPortsInFilterError
-
-
-class Ports(object):
-    def __init__(self, logger, table=None):
-        """
-
-        :param logging.Logger logger:
-        :param str table:
-        """
-        self._logger = logger
-        self.ports_list = []
-
-        if table is not None:
-            self.update_ports_from_table(table)
-
-    def __iter__(self):
-        return iter(self.ports_list)
-
-    def update_ports_from_table(self, table):
-        """Update ports from show filters output
-
-        :param str table:
-        :return:
-        """
-        lines = table.splitlines()
-        try:
-            if "no entries found" in lines[0].lower():
-                dicts = []
-            else:
-                dicts = table2dicts(lines[0], lines[1], lines[2:])
-        except ParseTableError:
-            self._logger.exception("Unable to parse ports: ")
-            raise ParseFilterError("Could not parse ports")
-
-        self.ports_list = []
-        for dict_ in dicts:
-            try:
-                port_ = Port.from_dict(dict_)
-            except UnsupportedPortsInFilterError:
-                # fixme really?
-                # fixme just ignore?
-                self._logger.debug(
-                    "We support only one port in the filter. Line was: \n {}".format(dict_.original_line)
-                )
-            else:
-                self.ports_list.append(port_)
+            :param str port:
+            :return:
+            """
+            if not self.PORT_PATTERN.match(port):
+                raise UnsupportedPortsInFilterError
